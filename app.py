8bdaba6e45fa0946c775bb085406d25c0a973686
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-07-07 15:35:43 +0100

    add initial flask set up & base.html

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 from flask import Flask, url_for, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
