3d9e472c5522b935412ac62f7cd3e469a6b9fa4c
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-07-10 12:00:26 +0100

    add navbar/link feature all pages

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template("index.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/reviews')
+def reviews():
+    return render_template("reviews.html")
+
+@app.route('/wish_list')
+def wish_list():
+    return render_template("wish_list.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
