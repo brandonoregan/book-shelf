92c8bfbb2c26b7dab967e0c3b2e2bd2e2c32b5d3
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-07-11 09:32:03 +0100

    add register and login feature

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ from flask import Flask, url_for, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
-    return render_template("index.html")
+users = {}
 
-@app.route('/login')
+@app.route('/', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        users[username] = password
+        print(users)
+        return redirect('login')
+
+
+    return render_template("index.html", user='users')
+
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username in users and users[username] == password:
+            return redirect('home')
     return render_template("login.html")
 
 @app.route('/home')
