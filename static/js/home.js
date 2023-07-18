6a5502e4f4ab4f0e2f4ca3664cbde4aee1d8d13f
Brandon O'Regan <brandon_oregan97@hotmail.com>
2023-07-18 12:34:10 +0100

    remove save changes buttons on  appropriate modals

--- static/js/home.js
+++ static/js/home.js
@@ -1,16 +1,12 @@
 "use strict";
 
-const bookMonthTitle = document.querySelector('.bookMonthTitle');
-const bookMonth = document.querySelector('.bookMonth');
+const bookMonthTitle = document.querySelector(".bookMonthTitle");
+const bookMonth = document.querySelector(".bookMonth");
 
-bookMonthTitle.addEventListener('click', function() {
-    bookMonthTitle.classList.toggle('bg-dark')
-})
+// bookMonthTitle.addEventListener('click', function() {
+//     bookMonthTitle.classList.toggle('bg-dark')
+// })
 
-bookMonth.addEventListener('click', function() {
-    bookMonth.classList.toggle('bg-dark')
-})
-
-
-
-
+// bookMonth.addEventListener('click', function() {
+//     bookMonth.classList.toggle('bg-dark')
+// })
